2e8f40e4891f99ad33d1f69b05eb3594086ff4da
estacimo <w0825062@myscc.ca>
2023-10-08 23:47:29 -0400

    Assignment 4

--- js/script.js
+++ js/script.js
@@ -1,49 +1,68 @@
-function JSONPage()
-{
-   $.getJSON('team.json', function(team)
-    {
-      $.each(team, function(index,value)
-        { 
-             let name = $('<h2>' + value.name + '</h2>');
-             let position = $('<h5>' + value.position + '</h5>');
-             let bio = $('<p>' + value.bio + '</p>');
-             $('#team').append(name, position, bio);
-        });
-    });
-}
-function retrieveData() 
-{
-   $.ajax({
-        type: "GET", 
-        url: "team.json",
-        dataType: "json",
-        timeout: 3000, 
-       
-        beforeSend: function () 
-        {
-            $("#team").text("Loading...");
-        },
-     
-        error: function () 
-        {
-            $("#team").text("Error: content is unretrievable.");
-        },
-     
-        success: function (team) 
-        {
-            $('#team').empty();
-            $.each(team, function (index, value) 
-            {
-                let name = $("<h2>" + value.name + "</h2>");
-                let position = $("<h5>" + value.position + "</h5>");
-                let bio = $("<p>" + value.bio + "</p>");
- 
-                $("#team").append(name, position, bio);
-            });
-        }
-    });
-}
+/*
+    Assignment #4
+    Esta Cimo
+*/
 
-$(function() {
-    retrieveData();
+$(function () {
+	function success(position) {
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		var locationHere = $("#locationhere");
+		var welcomeMessage = $("#welcome");
+		var distanceMessage = $("#distance");
+		var storedLocation = localStorage.getItem("lastLocation");
+		
+		locationHere.text(latitude + " " + longitude);
+		
+		if (storedLocation) {
+			var lastLocation = JSON.parse(storedLocation);
+			var distance = calcDistanceBetweenPoints(lastLocation.latitude, lastLocation.longitude, latitude, longitude);
+			
+			var storedLocationText = $("<p>").text("Last location: " + lastLocation.latitude + " " + lastLocation.longitude);
+			locationHere.after(storedLocationText);
+			
+			welcomeMessage.text("Welcome back!");
+			
+			distanceMessage.text("Distance travelled: " + distance + " meters.");		
+		} else {
+			welcomeMessage.text("Welcome to the application!");
+		}
+		
+		var currentLocation = { latitude: latitude, longitude: longitude };
+        localStorage.setItem("lastLocation", JSON.stringify(currentLocation));
+	}
+	
+	function fail(error) {
+		var locationHere = $("#locationhere");
+		
+		locationHere.text("Error.");
+	}
+
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(success, fail);
+	} else {
+		var locationHere = $("#locationhere");
+		
+		locationHere.text("Please allow geolocation in order to use this application.");
+	}
+
+    // DO NOT EDIT ANY CODE IN THIS FUNCTION DEFINTION
+    // function to calculate the distance in metres between two lat/long pairs on Earth
+    // Haversine formula - https://en.wikipedia.org/wiki/Haversine_formula
+    // Aren't those cool variable names? Yah gotta love JavaScript
+    function calcDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
+        var toRadians = function (num) {
+            return num * Math.PI / 180;
+        }
+        var R = 6371000; // radius of Earth in metres
+        var φ1 = toRadians(lat1);
+        var φ2 = toRadians(lat2);
+        var Δφ = toRadians(lat2 - lat1);
+        var Δλ = toRadians(lon2 - lon1);
+
+        var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (R * c);
+    }
 });
